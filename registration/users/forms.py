7b19a2c562c9dda821b50306020e03d4e1d6204f
AhmadReza Mozafari <74506045+AhmadRezaMozafari@users.noreply.github.com>
2024-07-12 16:36:46 +0330

    Update forms.py

--- registration/users/forms.py
+++ registration/users/forms.py
@@ -7,10 +7,3 @@ class RegisterForm(UserCreationForm):
         fields=[
             'first_name','email','username','password1','password2'
         ]
-
-        
-        def __init__(self, *args, **kwargs):
-            super(UserCreationForm, self).__init__(*args, **kwargs)
-
-            for name, filed in self.fields.items():
-                filed.widget.attrs.update({'class': 'form-styling'})
